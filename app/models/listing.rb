e0dd49188f527d2b5643d878c331b0d1f918a752
unknown <rossp@ross-NB.mip.co.za>
2015-01-09 00:07:08 +0200

    Heroku issue

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,10 +5,10 @@ class Listing < ActiveRecord::Base
                       :default_url => "default.jpg"
   else   
     has_attached_file :image, 
-                      :styles => { :medium => "200x200", :thumb => "100x100" }, 
+                      # :styles => { :medium => "200x200", :thumb => "100x100" }, 
                       :default_url => "default.jpg",
                       :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                       :path => ":style/:id_:filename"
   end
   
