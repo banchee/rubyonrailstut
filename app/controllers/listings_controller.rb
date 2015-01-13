2824a36484a63ca93b4698c60f04c23c859ce982
unknown <rossp@ross-NB.mip.co.za>
2015-01-13 10:43:44 +0200

    small fixes

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -24,8 +24,8 @@ class ListingsController < ApplicationController
   # POST /listings
   # POST /listings.json
   def create
-    @listing = Listing.new(listing_params)
-
+    @listing         = Listing.new(listing_params)
+    @listing.user_id = current_user.id
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
